3d3daa91aa562a053f415594d1e5fa6835a588de
dance2die <dontbothermewithspam@gmail.com>
2016-10-26 21:10:20 -0400

    Implemented Navbar

---
+++ Demo.WebSite.Mvc.Routing/Content/Shared/_HomeLayout.js
@@ -0,0 +1,15 @@
+﻿$(function() {
+	//$(".nav a").on("click", function (e) {
+	//	$(".nav").find(".active").removeClass("active");
+	//	$(this).parent().addClass("active");
+	//});
+
+	// http://stackoverflow.com/a/19310428/4035
+	var url = window.location;
+	$('.navbar .nav').find('.active').removeClass('active');
+	$('.navbar .nav li a').each(function () {
+		if (this.href == url) {
+			$(this).parent().addClass('active');
+		}
+	});
+});
